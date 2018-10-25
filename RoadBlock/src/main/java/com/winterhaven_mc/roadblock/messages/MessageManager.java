5e38af124a8d9f05847537978a1724859552a85b
Tim Savage <tim.savage@gmail.com>
2018-10-25 15:33:26 -0500

    update to use MessageManagerLib

--- RoadBlock/src/main/java/com/winterhaven_mc/roadblock/messages/MessageManager.java
+++ RoadBlock/src/main/java/com/winterhaven_mc/roadblock/messages/MessageManager.java
@@ -1,11 +1,14 @@
 package com.winterhaven_mc.roadblock.messages;
 
 import com.winterhaven_mc.roadblock.PluginMain;
-import com.winterhaven_mc.roadblock.utilities.AbstractMessageManager;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import util.AbstractMessageManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -17,9 +20,30 @@ import org.bukkit.command.CommandSender;
  */
 public final class MessageManager extends AbstractMessageManager {
 
-	// inherit constructor from super class
+
+	/**
+	 * Constructor
+	 * @param plugin reference to main class
+	 */
 	public MessageManager(PluginMain plugin) {
-		super(plugin);
+
+		// call super class constructor
+		//noinspection unchecked
+		super(plugin, MessageId.class);
+	}
+
+
+	@Override
+	protected Map<String,String> getDefaultReplacements(CommandSender recipient) {
+
+		Map<String,String> replacements = new HashMap<>();
+		replacements.put("%PLAYER_NAME%",recipient.getName());
+		replacements.put("%WORLD_NAME%",ChatColor.stripColor(getWorldName(recipient)));
+		replacements.put("%TOOL_NAME%", ChatColor.stripColor(getToolName()));
+		replacements.put("%QUANTITY%","1");
+		replacements.put("%MATERIAL%","unknown");
+
+		return replacements;
 	}
 
 
@@ -32,7 +56,12 @@ public final class MessageManager extends AbstractMessageManager {
 	public final void sendMessage(final CommandSender recipient,
 								  final MessageId messageId) {
 
-		sendMessage(recipient, messageId, 1, null);
+		// get default replacements map
+		Map<String,String> replacements = getDefaultReplacements(recipient);
+
+		// send message to recipient
+		//noinspection unchecked
+		sendMessage(recipient, messageId, replacements);
 	}
 
 
@@ -45,9 +74,17 @@ public final class MessageManager extends AbstractMessageManager {
 	 */
 	public final void sendMessage(final CommandSender recipient,
 								  final MessageId messageId,
-								  final int quantity) {
+								  final Integer quantity) {
 
-		sendMessage(recipient, messageId, quantity, null);
+		// get default replacements map
+		Map<String,String> replacements = getDefaultReplacements(recipient);
+
+		// set passed quantity in replacements map
+		replacements.put("%QUANTITY%",quantity.toString());
+
+		// send message to recipient
+		//noinspection unchecked
+		sendMessage(recipient, messageId, replacements);
 	}
 
 
@@ -62,65 +99,51 @@ public final class MessageManager extends AbstractMessageManager {
 								  final MessageId messageId,
 								  final Material material) {
 
-		sendMessage(recipient, messageId, 1, material);
+		// get default replacements map
+		Map<String,String> replacements = getDefaultReplacements(recipient);
+
+		// set name of passed material in replacements map
+		replacements.put("%MATERIAL%",material.toString());
+
+		// send message to recipient
+		//noinspection unchecked
+		sendMessage(recipient, messageId, replacements);
 	}
 
 
 	/**
-	 *  Send message to player
-	 *
-	 * @param recipient			player receiving message
-	 * @param messageId			message identifier in messages file
-	 * @param quantity			quantity
-	 * @param material			material type to be referenced in message
+	 * Get custom tool name from language file
+	 * @return the custom tool name string
 	 */
-	@SuppressWarnings("WeakerAccess")
-	public final void sendMessage(final CommandSender recipient,
-	                                    final MessageId messageId,
-										final Integer quantity,
-										final Material material) {
+	public final String getToolName() {
 
+		// get tool name from language file
+		String toolName = messages.getString("tool_info.TOOL_NAME");
 
-		// if message is not enabled in messages file, do nothing and return
-		if (!isEnabled(messageId)) {
-			if (plugin.debug) {
-				plugin.getLogger().info("Message " + messageId.toString() + "disabled, not displayed.");
-			}
-			return;
+		// if fetching tool name failed, use plugin name
+		if (toolName == null) {
+			toolName = plugin.getName() + " Tool";
 		}
 
-		// if message is not cooled, do nothing and return
-		if (!isCooled(recipient,messageId)) {
-			return;
-		}
-
-		String materialName = "unknown";
-
-		// if passed material is not null, set materialName
-		if (material != null) {
-			materialName = material.toString();
-		}
-
-		// get recipient's current world name
-		String worldName = getWorldName(recipient);
-
-		// use Multiverse alias for world name if available
-		worldName = plugin.worldManager.getWorldName(worldName);
-
-		// get message from file
-		String message = getMessage(messageId);
-
-		// do variable substitutions *
-		if (message.contains("%")) {
-			message = replace(message,"%PLAYER_NAME%",recipient.getName());
-			message = replace(message,"%WORLD_NAME%", ChatColor.stripColor(worldName));
-			message = replace(message,"%TOOL_NAME%",ChatColor.stripColor(getToolName()));
-			message = replace(message,"%QUANTITY%", quantity.toString());
-			message = replace(message,"%MATERIAL%", materialName);
-		}
-
-		// send message to player
-		recipient.sendMessage(ChatColor.translateAlternateColorCodes('&',message));
+		// replace alt color code character
+		toolName = ChatColor.translateAlternateColorCodes('&', toolName);
+		return toolName;
 	}
+
+
+	/**
+	 * Get custom tool lore from language file
+	 * @return the custom tool lore as a List of String
+	 */
+	public final List<String> getToolLore() {
+		List<String> lore = messages.getStringList("tool_info.TOOL_LORE");
+		int lineNumber = 0;
+		while (lineNumber < lore.size()) {
+			lore.set(lineNumber, ChatColor.translateAlternateColorCodes('&',lore.get(lineNumber)));
+			lineNumber++;
+		}
+		return lore;
+	}
+
 
 }
