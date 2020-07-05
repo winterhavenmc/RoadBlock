fbb5b3fe96fe7d7f96085e6bd40dba9ee41f9c9d
Tim Savage <tim.savage@gmail.com>
2020-07-05 05:27:33 -0500

    add javadocs

--- RoadBlock/src/main/java/com/winterhaven_mc/roadblock/messages/Message.java
+++ RoadBlock/src/main/java/com/winterhaven_mc/roadblock/messages/Message.java
@@ -7,10 +7,24 @@ import org.bukkit.command.CommandSender;
 public class Message extends AbstractMessage<MessageId, Macro> {
 
 
+	/**
+	 * Private class constructor; calls inherited super constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 */
 	private Message(CommandSender recipient, MessageId messageId) {
 		super(recipient, messageId);
 	}
 
+
+	/**
+	 * Static class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 * @return new instance of Message created with private constructor
+	 */
 	public static Message create(CommandSender recipient, MessageId messageId) {
 		return new Message(recipient, messageId);
 	}
