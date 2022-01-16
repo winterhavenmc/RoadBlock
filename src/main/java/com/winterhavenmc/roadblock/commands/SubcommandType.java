4913fb766a771996619f05de0f067cc889c7e6d1
Tim Savage <tim.savage@gmail.com>
2022-01-16 13:36:11 -0600

    de-couple subcommandRegistry from SubcommandType

--- src/main/java/com/winterhavenmc/roadblock/commands/SubcommandType.java
+++ src/main/java/com/winterhavenmc/roadblock/commands/SubcommandType.java
@@ -5,49 +5,41 @@ import com.winterhavenmc.roadblock.PluginMain;
 
 public enum SubcommandType {
 
-	HELP() {
-		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new HelpCommand(plugin, subcommandRegistry));
-		}
-	},
-
 	MATERIALS() {
 		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new MaterialsCommand(plugin));
+		Subcommand create(final PluginMain plugin) {
+			return new MaterialsCommand(plugin);
 		}
 	},
 
 	RELOAD() {
 		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new ReloadCommand(plugin));
+		Subcommand create(final PluginMain plugin) {
+			return new ReloadCommand(plugin);
 		}
 	},
 
 	SHOW() {
 		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new ShowCommand(plugin));
+		Subcommand create(final PluginMain plugin) {
+			return new ShowCommand(plugin);
 		}
 	},
 
 	STATUS() {
 		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new StatusCommand(plugin));
+		Subcommand create(final PluginMain plugin) {
+			return new StatusCommand(plugin);
 		}
 	},
 
 	TOOL() {
 		@Override
-		void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry) {
-			subcommandRegistry.register(new ToolCommand(plugin));
+		Subcommand create(final PluginMain plugin) {
+			return new ToolCommand(plugin);
 		}
 	};
 
-
-	abstract void register(final PluginMain plugin, final SubcommandRegistry subcommandRegistry);
+	abstract Subcommand create(final PluginMain plugin);
 
 }
