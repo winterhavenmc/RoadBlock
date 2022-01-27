0d27d9fafbb57708db4bed22575d4e766f4daa11
Tim Savage <tim.savage@gmail.com>
2022-01-26 20:25:49 -0600

    add javadoc

--- src/main/java/com/winterhavenmc/roadblock/highlights/RemoveHighlightTask.java
+++ src/main/java/com/winterhavenmc/roadblock/highlights/RemoveHighlightTask.java
@@ -6,12 +6,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 
+/**
+ * A class that extends BukkitRunnable to execute a task after a delay to remove highlighting from blocks for a player
+ */
 final class RemoveHighlightTask extends BukkitRunnable {
 
 	private final PluginMain plugin;
 	private final Player player;
 
 
+	/**
+	 * Class constructor
+	 *
+	 * @param plugin a reference to the plugin main class
+	 * @param player the player for whom to remove block highlighting
+	 */
 	RemoveHighlightTask(final PluginMain plugin, final Player player) {
 		this.plugin = plugin;
 		this.player = player;
