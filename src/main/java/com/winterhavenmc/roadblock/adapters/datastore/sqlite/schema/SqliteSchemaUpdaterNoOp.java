4d7193042e710d7f5bb62805a6fa48b304526d17
Tim Savage <tim.savage@gmail.com>
2025-08-20 23:46:10 -0500

    replace error message strings with enum constants

--- src/main/java/com/winterhavenmc/roadblock/adapters/datastore/sqlite/schema/SqliteSchemaUpdaterNoOp.java
+++ src/main/java/com/winterhavenmc/roadblock/adapters/datastore/sqlite/schema/SqliteSchemaUpdaterNoOp.java
@@ -1,22 +1,27 @@
 package com.winterhavenmc.roadblock.adapters.datastore.sqlite.schema;
 
+import com.winterhavenmc.library.messagebuilder.resources.configuration.LocaleProvider;
+import com.winterhavenmc.roadblock.adapters.datastore.sqlite.SqliteMessage;
 import org.bukkit.plugin.Plugin;
 
 
 public final class SqliteSchemaUpdaterNoOp implements SqliteSchemaUpdater
 {
 	private final Plugin plugin;
+	private final LocaleProvider localeProvider;
 
-	public SqliteSchemaUpdaterNoOp(final Plugin plugin)
+
+	public SqliteSchemaUpdaterNoOp(final Plugin plugin, final LocaleProvider localeProvider)
 	{
 		this.plugin = plugin;
+		this.localeProvider = localeProvider;
 	}
 
 
 	@Override
 	public void update()
 	{
-		plugin.getLogger().info("SQLite schema is up to date.");
+		plugin.getLogger().info(SqliteMessage.SCHEMA_UP_TO_DATE_NOTICE.getLocalizedMessage(localeProvider.getLocale()));
 	}
 
 }
