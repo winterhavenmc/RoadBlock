fb1b3fc4824b6df24ad3d26ed88bacd16fb57f14
Tim Savage <tim.savage@gmail.com>
2025-11-07 22:55:04 -0600

    rename method

--- adapters/src/main/java/com/winterhavenmc/roadblock/adapters/datastore/sqlite/SqliteBlockRepository.java
+++ adapters/src/main/java/com/winterhavenmc/roadblock/adapters/datastore/sqlite/SqliteBlockRepository.java
@@ -1,11 +1,16 @@
 package com.winterhavenmc.roadblock.adapters.datastore.sqlite;
 
 import com.winterhavenmc.library.messagebuilder.models.configuration.ConfigRepository;
+import com.winterhavenmc.roadblock.core.ports.config.MaterialsProvider;
+import com.winterhavenmc.roadblock.core.util.Config;
 import com.winterhavenmc.roadblock.models.blocklocation.BlockLocation;
 import com.winterhavenmc.roadblock.core.ports.datastore.BlockRepository;
 
 import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
@@ -13,29 +18,36 @@ import org.bukkit.plugin.Plugin;
 
 import java.sql.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class SqliteBlockRepository implements BlockRepository, Listener
 {
 	private final Plugin plugin;
-	private final ConfigRepository configRepository;
 	private final Connection connection;
-	private final SqliteBlockQueryExecutor blockQueryHelper = new SqliteBlockQueryExecutor();
+	private final ConfigRepository configRepository;
+	private final MaterialsProvider materialsProvider;
 	private final SqliteBlockRowMapper blockRowMapper;
 	private final int schemaVersion;
 	private final BlockLocationCache blockCache;
 	private final Collection<Location> chunkCache;
+	private final SqliteBlockQueryExecutor blockQueryExecutor;
 
 
-	public SqliteBlockRepository(final Plugin plugin, final Connection connection, final ConfigRepository configRepository)
+	public SqliteBlockRepository(final Plugin plugin,
+	                             final Connection connection,
+	                             final ConfigRepository configRepository,
+	                             final MaterialsProvider materialsProvider)
 	{
 		this.plugin = plugin;
-		this.configRepository = configRepository;
 		this.connection = connection;
+		this.configRepository = configRepository;
+		this.materialsProvider = materialsProvider;
 		this.blockCache = BlockLocationCache.getInstance();
 		this.chunkCache = new HashSet<>();
 		this.schemaVersion = getSchemaVersion();
 		this.blockRowMapper = new SqliteBlockRowMapper(plugin, configRepository);
+		this.blockQueryExecutor = new SqliteBlockQueryExecutor();
 
 		// register events in this class
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -70,6 +82,7 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 	 *
 	 * @param blockLocations Set of records to insert
 	 */
+	@Override
 	public int save(final Set<BlockLocation.Valid> blockLocations)
 	{
 		try (PreparedStatement preparedStatement = connection.prepareStatement(SqliteQueries.getQuery("InsertOrIgnoreBlock")))
@@ -102,11 +115,12 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 	 *
 	 * @return Set of location records
 	 */
+	@Override
 	public Set<BlockLocation.Valid> getAll()
 	{
 		try (PreparedStatement preparedStatement = connection.prepareStatement(SqliteQueries.getQuery("SelectAllBlocks")))
 		{
-			ResultSet resultSet = blockQueryHelper.selectAllRecords(preparedStatement);
+			ResultSet resultSet = blockQueryExecutor.selectAllRecords(preparedStatement);
 
 			return blockRowMapper.mapLocations(resultSet, schemaVersion);
 		}
@@ -158,7 +172,7 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 	{
 		try (PreparedStatement preparedStatement = connection.prepareStatement(SqliteQueries.getQuery("SelectBlocksInChunk")))
 		{
-			ResultSet resultSet = blockQueryHelper.selectRecordsInChunk(chunk, preparedStatement);
+			ResultSet resultSet = blockQueryExecutor.selectRecordsInChunk(chunk, preparedStatement);
 			return blockRowMapper.mapLocations(resultSet, schemaVersion);
 		}
 		catch (SQLException sqlException)
@@ -186,7 +200,7 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 		{
 			try (PreparedStatement preparedStatement = connection.prepareStatement(SqliteQueries.getQuery("SelectNearbyBlocks")))
 			{
-				ResultSet resultSet = blockQueryHelper.selectNearbyBlocks(validBlockLocation, distance, preparedStatement);
+				ResultSet resultSet = blockQueryExecutor.selectNearbyBlocks(validBlockLocation, distance, preparedStatement);
 				while (resultSet.next())
 				{
 					final double x = resultSet.getDouble("x");
@@ -208,6 +222,7 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 	}
 
 
+	@Override
 	public int delete(final Set<BlockLocation.Valid> blockLocations)
 	{
 		int count = 0;
@@ -222,7 +237,7 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 				{
 					try
 					{
-						blockQueryHelper.deleteRecords(validLocation, preparedStatement);
+						blockQueryExecutor.deleteRecords(validLocation, preparedStatement);
 						count += 1;
 					}
 					catch (SQLException sqlException)
@@ -333,6 +348,183 @@ public class SqliteBlockRepository implements BlockRepository, Listener
 	public void onChunkUnload(ChunkUnloadEvent event)
 	{
 		flushCache(event.getChunk());
+	}
+
+
+
+	/**
+	 * Remove block locations from datastore
+	 *
+	 * @param locations a Collection of Locations to be deleted from the datastore
+	 */
+	@Override
+	public int removeBlockLocations(final Collection<Location> locations)
+	{
+		return delete(getBlockLocations(locations));
+	}
+
+
+	/**
+	 * Returns a Set of valid block locations from a Collection of Bukkit locations.
+	 */
+	public Set<BlockLocation.Valid> getBlockLocations(final Collection<Location> locations)
+	{
+		return locations.stream()
+				.map(BlockLocation::of)
+				.filter(BlockLocation.Valid.class::isInstance)
+				.map(BlockLocation.Valid.class::cast)
+				.collect(Collectors.toSet());
+	}
+
+
+	/**
+	 * Insert block location records into datastore
+	 *
+	 * @param locations a Collection of Locations to be inserted into the datastore
+	 */
+	@Override
+	public int storeBlockLocations(final Collection<Location> locations)
+	{
+		return save(getBlockLocations(locations));
+	}
+
+
+	/**
+	 * Create Set of all blocks of valid road block material attached to location
+	 *
+	 * @param startLocation location to begin searching for attached road blocks
+	 * @return Set of Locations of attached road blocks
+	 */
+	@Override
+	public Set<Location> getFill(final Location startLocation, final MaterialsProvider materialsProvider)
+	{
+		if (startLocation == null) return Collections.emptySet();
+
+		final Set<Location> returnSet = new HashSet<>();
+		final Queue<Location> queue = new LinkedList<>();
+
+		// put start location in queue
+		queue.add(startLocation);
+		while (!queue.isEmpty())
+		{
+			// remove location at head of queue
+			Location loc = queue.poll();
+
+			// if location is not in return set and is a road block material and is not too far from start...
+			if (!returnSet.contains(loc) && materialsProvider.contains(loc.getBlock().getType())
+					&& loc.distanceSquared(startLocation) < Math.pow(Config.SPREAD_DISTANCE.getInt(plugin.getConfig()), 2))
+			{
+				// add location to return set
+				returnSet.add(loc);
+
+				// add adjacent locations to queue
+				queue.add(loc.clone().add(0, 0, 1));
+				queue.add(loc.clone().add(0, 0, -1));
+				queue.add(loc.clone().add(1, 0, 0));
+				queue.add(loc.clone().add(-1, 0, 0));
+			}
+		}
+		return returnSet;
+	}
+
+
+
+	/**
+	 * Check if block below player is a protected road block
+	 *
+	 * @param player the player to is above a road block
+	 * @return {@code true} if player is within three blocks above a road block, else {@code false}
+	 */
+	@Override
+	public boolean isAboveRoad(final Player player)
+	{
+		// if player is null, return false
+		if (player == null)
+		{
+			return false;
+		}
+
+		// get configured height above road
+		final int distance = Config.ON_ROAD_HEIGHT.getInt(plugin.getConfig());
+
+		// if distance is less than one, return false
+		if (distance < 1)
+		{
+			return false;
+		}
+
+		// return result of isAboveRoad for player location and configured height
+		return isAboveRoad(player.getLocation(), distance);
+	}
+
+
+	/**
+	 * Check if block below location is a protected road block, searching down to maxDepth
+	 *
+	 * @param location the location to test if above a road block
+	 * @param distance the distance in blocks to test below location for road blocks
+	 * @return {@code true} if location is above a road block, else {@code false}
+	 */
+	@Override
+	public boolean isAboveRoad(final Location location, final int distance)
+	{
+		// if passed location is null, return false
+		if (location == null)
+		{
+			return false;
+		}
+
+		// if passed distance is less than one, return false
+		if (distance < 1)
+		{
+			return false;
+		}
+
+		boolean result = false;
+		int checkDepth = distance;
+
+		// iterate until maxDepth reached
+		while (checkDepth > 0)
+		{
+			// get block at checkDepth
+			Block testBlock = location.getBlock().getRelative(BlockFace.DOWN, checkDepth);
+
+			// don't check datastore unless testBlock is road block material
+			if (materialsProvider.isRoadBlockMaterial(testBlock))
+			{
+				if (isProtected(testBlock.getLocation()))
+				{
+					result = true;
+					break;
+				}
+			}
+
+			// decrement checkDepth
+			checkDepth--;
+		}
+		return result;
+	}
+
+
+	/**
+	 * Check if block is a protected road block
+	 *
+	 * @param block the block to test
+	 * @return {@code true} if the block is a protected road block, else {@code false}
+	 */
+	@Override
+	public boolean isRoadBlock(final Block block)
+	{
+		if (block == null) return false;
+
+		// check if block is road block material
+		if (!materialsProvider.isRoadBlockMaterial(block))
+		{
+			return false;
+		}
+
+		// check if block is in cache or datastore
+		return isProtected(block.getLocation());
 	}
 
 }
