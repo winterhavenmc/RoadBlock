1138f08ccf173f68b2b8427be99988b5268e5c73
Tim Savage <tim.savage@gmail.com>
2025-11-07 22:49:15 -0600

    instantiate objects need for PluginController.startUp and SimpleApi

--- plugin/src/main/java/com/winterhavenmc/roadblock/plugin/Bootsrap.java
+++ plugin/src/main/java/com/winterhavenmc/roadblock/plugin/Bootsrap.java
@@ -1,25 +1,45 @@
 package com.winterhavenmc.roadblock.plugin;
 
+import com.winterhavenmc.library.messagebuilder.MessageBuilder;
 import com.winterhavenmc.roadblock.adapters.datastore.sqlite.SqliteConnectionProvider;
 
+import com.winterhavenmc.roadblock.adapters.listeners.bukkit.BukkitBlockEventListener;
 import com.winterhavenmc.roadblock.core.controller.PluginController;
 import com.winterhavenmc.roadblock.core.controller.RoadBlockPluginController;
+import com.winterhavenmc.roadblock.adapters.highlights.bukkit.BukkitHighlightManager;
+import com.winterhavenmc.roadblock.core.ports.highlights.HighlightManager;
+import com.winterhavenmc.roadblock.adapters.listeners.bukkit.BukkitEntityEventListener;
+import com.winterhavenmc.roadblock.core.ports.config.MaterialsProvider;
+import com.winterhavenmc.roadblock.core.ports.datastore.BlockRepository;
 import com.winterhavenmc.roadblock.core.ports.datastore.ConnectionProvider;
+import com.winterhavenmc.roadblock.core.util.SimpleApi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bootsrap extends JavaPlugin
 {
 	private PluginController pluginController;
+	private static SimpleApi simpleApi;
 
 
 	@Override
 	public void onEnable()
 	{
-		final ConnectionProvider connectionProvider = new SqliteConnectionProvider(this);
 		pluginController = new RoadBlockPluginController();
 
-		pluginController.startUp(this, connectionProvider);
+		final MaterialsProvider materials = new MaterialsProvider(this);
+		final ConnectionProvider connectionProvider = new SqliteConnectionProvider(this, materials);
+		final BlockRepository blocks = connectionProvider.blocks();
+
+		final MessageBuilder messageBuilder = MessageBuilder.create(this);
+		final HighlightManager highlightManager = new BukkitHighlightManager(this);
+
+		new BukkitBlockEventListener(this, messageBuilder, blocks);
+		new BukkitEntityEventListener(this, messageBuilder, blocks, materials, highlightManager);
+
+		pluginController.startUp(this, messageBuilder, highlightManager, connectionProvider, materials);
+
+		Bootsrap.simpleApi = new SimpleApi(this, blocks, materials);
 	}
 
 
@@ -27,6 +47,13 @@ public class Bootsrap extends JavaPlugin
 	public void onDisable()
 	{
 		pluginController.shutDown();
+	}
+
+
+	@SuppressWarnings("unused")
+	public static SimpleApi simpleApi()
+	{
+		return Bootsrap.simpleApi;
 	}
 
 }
