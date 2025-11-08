8207013c54ac0689a7ca306d4e2cac200c242169
Tim Savage <tim.savage@gmail.com>
2025-11-08 08:50:44 -0600

    delete obsolete class

--- core/src/main/java/com/winterhavenmc/roadblock/core/context/MetricsCtx.java
+++
@@ -1,8 +0,0 @@
-package com.winterhavenmc.roadblock.core.context;
-
-import com.winterhavenmc.roadblock.core.ports.datastore.BlockRepository;
-
-import org.bukkit.plugin.Plugin;
-
-
-public record MetricsCtx(Plugin plugin, BlockRepository blocks) { }
