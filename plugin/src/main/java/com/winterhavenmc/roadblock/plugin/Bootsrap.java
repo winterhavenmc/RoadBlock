25b43f97bb7cd5a0360d6dd6b20ba359d8bd3370
Tim Savage <tim.savage@gmail.com>
2025-11-08 08:57:12 -0600

    refactor to remove PluginController class

--- plugin/src/main/java/com/winterhavenmc/roadblock/plugin/Bootsrap.java
+++ plugin/src/main/java/com/winterhavenmc/roadblock/plugin/Bootsrap.java
@@ -1,52 +1,57 @@
 package com.winterhavenmc.roadblock.plugin;
 
-import com.winterhavenmc.library.messagebuilder.MessageBuilder;
+import com.winterhavenmc.roadblock.adapters.commands.bukkit.BukkitCommandDispatcher;
 import com.winterhavenmc.roadblock.adapters.datastore.sqlite.SqliteConnectionProvider;
-
-import com.winterhavenmc.roadblock.adapters.listeners.bukkit.BukkitBlockEventListener;
-import com.winterhavenmc.roadblock.core.controller.PluginController;
-import com.winterhavenmc.roadblock.core.controller.RoadBlockPluginController;
 import com.winterhavenmc.roadblock.adapters.highlights.bukkit.BukkitHighlightManager;
-import com.winterhavenmc.roadblock.core.ports.highlights.HighlightManager;
+import com.winterhavenmc.roadblock.adapters.listeners.bukkit.BukkitBlockEventListener;
 import com.winterhavenmc.roadblock.adapters.listeners.bukkit.BukkitEntityEventListener;
+
+import com.winterhavenmc.roadblock.core.util.PluginCtx;
+import com.winterhavenmc.roadblock.core.ports.highlights.HighlightManager;
 import com.winterhavenmc.roadblock.core.ports.config.MaterialsProvider;
 import com.winterhavenmc.roadblock.core.ports.datastore.BlockRepository;
 import com.winterhavenmc.roadblock.core.ports.datastore.ConnectionProvider;
+import com.winterhavenmc.roadblock.core.util.MetricsHandler;
 import com.winterhavenmc.roadblock.core.util.SimpleApi;
+
+import com.winterhavenmc.library.messagebuilder.MessageBuilder;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class Bootsrap extends JavaPlugin
 {
-	private PluginController pluginController;
+	private ConnectionProvider connectionProvider;
 	private static SimpleApi simpleApi;
 
 
 	@Override
 	public void onEnable()
 	{
-		pluginController = new RoadBlockPluginController();
-
-		final MaterialsProvider materials = new MaterialsProvider(this);
-		final ConnectionProvider connectionProvider = new SqliteConnectionProvider(this, materials);
-		final BlockRepository blocks = connectionProvider.blocks();
+		// install default config.yml if not present
+		saveDefaultConfig();
 
 		final MessageBuilder messageBuilder = MessageBuilder.create(this);
+		final MaterialsProvider materials = new MaterialsProvider(this);
+		this.connectionProvider = SqliteConnectionProvider.connect(this, materials);
+		final BlockRepository blocks = connectionProvider.blocks();
 		final HighlightManager highlightManager = new BukkitHighlightManager(this);
 
-		new BukkitBlockEventListener(this, messageBuilder, blocks);
-		new BukkitEntityEventListener(this, messageBuilder, blocks, materials, highlightManager);
+		final PluginCtx ctx = new PluginCtx(this, messageBuilder, materials, blocks, highlightManager);
 
-		pluginController.startUp(this, messageBuilder, highlightManager, connectionProvider, materials);
+		new BukkitCommandDispatcher(ctx);
+		new BukkitBlockEventListener(ctx);
+		new BukkitEntityEventListener(ctx);
+		new MetricsHandler(ctx);
 
-		Bootsrap.simpleApi = new SimpleApi(this, blocks, materials);
+		Bootsrap.simpleApi = new SimpleApi(ctx);
 	}
 
 
 	@Override
 	public void onDisable()
 	{
-		pluginController.shutDown();
+		connectionProvider.close();
 	}
 
 
